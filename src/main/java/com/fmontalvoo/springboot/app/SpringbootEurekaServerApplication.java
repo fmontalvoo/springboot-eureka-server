bafa8d49ff9c29dcd2848b0a548c6b382b0c1989
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-09-23 18:16:28 -0500

    Initial commit
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ src/main/java/com/fmontalvoo/springboot/app/SpringbootEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.fmontalvoo.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEurekaServerApplication.class, args);
+	}
+
+}
