b8723a33d6550b272c2aa1ac9788a2e0614730f7
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-09-23 18:35:14 -0500

    Configuracion eureka server.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/main/java/com/fmontalvoo/springboot/app/SpringbootEurekaServerApplication.java
+++ src/main/java/com/fmontalvoo/springboot/app/SpringbootEurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.fmontalvoo.springboot.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class SpringbootEurekaServerApplication {
 
